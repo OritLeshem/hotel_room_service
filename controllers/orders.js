befbd37b5a2fa80ca3c2d780a77ea8bbdf09f171
Orit Leshem <orit@Orits-Mac-mini.ht.home>
2021-05-03 22:51:09 -0400

    include room data in confirm

--- controllers/orders.js
+++ controllers/orders.js
@@ -19,22 +19,27 @@ function create_order(req,res){
         if(err) return res.redirect('/orders');
         res.redirect(`/orders/${order._id}`)
     })
-    console.log(order);
+    // console.log(order);
 }
 
 function all_info(req, res) {
     Order.find({},function(err, orders){
     Room.find({}, function (err, r_num) {
-        console.log(r_num);
-        console.log(orders);
+        // console.log("r_num___>",r_num);
+        // console.log("orders--->",orders);
         res.render('all_info',{orders,r_num});
         });
     });
 }
 function confirm(req, res) {
     Order.findById((req.params.id),function(err, order) {
+        //console.log("req.body---->",order.roomNum)
+
     Room.find({room_num:order.roomNum}, function (err, r_num) {
-        console.log(r_num);
+        console.log("r_num[0]._id---->",r_num[0]._id)
+    order.room_data.push(r_num[0]._id);
+    order.save()
+        // console.log(r_num);
         res.render('confirm', {order,r_num})
    });
     });
