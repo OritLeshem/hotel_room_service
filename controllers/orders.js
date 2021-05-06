ec7a41082755ea3e8d771659e0a768485f4d3e99
Orit Leshem <orit@Orits-Mac-mini.ht.home>
2021-05-06 18:52:19 -0400

    final-e

--- controllers/orders.js
+++ controllers/orders.js
@@ -1,7 +1,7 @@
 const Order = require('../models/order');
 const Room = require('../models/room');
 
-module.exports={
+module.exports={ 
     create_order,
     start_menu,
     side_dish,
@@ -21,6 +21,7 @@ function start_menu(req,res){
     console.log("start menu");
     res.render("orders/main_dish");
 }
+
 function from_chef(req,res){
     console.log("start menu");
     res.render("from_chef");
@@ -33,23 +34,19 @@ function create_order(req,res){
         if(err) return res.send(err);
         res.redirect(`/orders/${order._id}`)
     })
-   
 }
+
 function edit_order(req,res){
     Order.findById((req.params.id),function(err, order) {
         res.render('orders/edit_order',{order});
     })
 }
 
-
 function edit_main_dish(req, res){
     Order.findByIdAndUpdate(req.params.id,{main_dish:req.body.main_dish},function(err,order){
-      
         res.redirect(`/orders/${order._id}`)
     })
-  }
-
-
+}
 
 function side_dish(req,res){
    
@@ -61,10 +58,10 @@ function side_dish(req,res){
 
 function add_side_dish(req, res){
     console.log("add_side_dish parms before---->",req.params)
-  Order.findByIdAndUpdate(req.params.id,{side_dish:req.body.side_dish},function(err,order){
-    console.log("add_side_dish parms after---->",req.params)
-    res.redirect(`/orders/${order._id}/dessert_dish`)
-    console.log("add_side_dish end order._id---->",order._id)
+    Order.findByIdAndUpdate(req.params.id,{side_dish:req.body.side_dish},function(err,order){
+        console.log("add_side_dish parms after---->",req.params)
+        res.redirect(`/orders/${order._id}/dessert_dish`)
+        console.log("add_side_dish end order._id---->",order._id)
   })
 }
 
@@ -80,86 +77,43 @@ function add_dessert_dish(req, res){
     Order.findByIdAndUpdate(req.params.id,{dessert_dish:req.body.dessert_dish,allergies:req.body.allergies },function(err,order){
         console.log("add_dessert_dish parms---->",req.params)
         res.redirect(`/orders/${order._id}/confirm`)        
-        // res.redirect(`/orders/${order._id}/dessert_dish`)
     });
 }
-
-  
 
 function all_info(req, res) {
     Order.find({}).
-    
     populate('room_data').
     exec(function (err,orders){
         if (err) return handleError(err);
-       
         res.render('all_info',{orders}); 
-          });
-        
-        
-    
-}
-
-
-// function all_info(req, res) {
-//     Order.find({},function(err, orders){
-//     Room.find({}, function (err, r_num) {
-//         // console.log("r_num___>",r_num);
-//         // console.log("orders--->",orders);
-//         res.render('all_info',{orders,r_num});
-//         });
-//     });
-// }
-
-
-function confirm(req, res) {
-    Order.findById((req.params.id),function(err, order) {
-        //console.log("req.body---->",order.roomNum)
-
-    Room.find({room_num:order.roomNum}, function (err, r_num) {
-        // console.log("r_num[0]._id---->",r_num[0]._id)
-    if(r_num[0]!==undefined){    
-    order.room_data.push(r_num[0]._id);
-    order.save()
-        // console.log(r_num);
-        res.render('confirm', {order,r_num})
-    }else{
-        res.render("room_err");
-    }
-   });
     });
 }
 
-// function confirm(req, res) {
-//     Order.findById((req.params.id),function(err, order) {
-//         //console.log("req.body---->",order.roomNum)
+function confirm(req, res) {
+    Order.findById((req.params.id),function(err, order) {
+          Room.find({room_num:order.roomNum}, function (err, r_num) {
+                if(r_num[0]!==undefined){    
+                 order.room_data.push(r_num[0]._id);
+                 order.save()
+                 res.render('confirm', {order,r_num})
+                }else{
+                res.render("room_err");
+                }
+            });
+    });
+}
 
-//     Room.find({room_num:order.roomNum}, function (err, r_num) {
-//         // console.log("r_num[0]._id---->",r_num[0]._id)
-//     order.room_data.push(r_num[0]._id);
-//     order.save()
-//         // console.log(r_num);
-//         res.render('confirm', {order,r_num})
-//    });
-//     });
-// }
 function delete_one_info(req,res){
     console.log("delete function wae reached",req.params.id)
     Order.deleteOne({_id:req.params.id},function(err, order){
         res.redirect('/orders/all_info')
     });
-    
-    
-
 }
 
 function delete_all_info(req,res){
     Order.deleteMany(function(err, order){
         res.redirect('/orders/all_info')
-
     });
-    
-
 }
 
      
